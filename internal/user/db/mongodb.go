72b08ec06aa8a3cb28d3276031c5501552d61108
fl35hdr0ppp <khusnutdinovakim@gmail.com>
2023-02-24 15:10:10 +0300

    added user model and user dto + added wrapper to connect to db + db user logic

---
+++ internal/user/db/mongodb.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"simple-rest-api/internal/user"
+	"simple-rest-api/pkg/logging"
+)
+
+type db struct {
+	collection *mongo.Collection
+	logger     *logging.Logger
+}
+
+func (d *db) Create(ctx context.Context, user user.User) (string, error) {
+	d.logger.Debug("create user")
+	result, err := d.collection.InsertOne(ctx, user)
+	if err != nil {
+		return "", fmt.Errorf("failed to create user: %v", err)
+	}
+
+	d.logger.Debug("convert inserted id to objID")
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if ok {
+		return oid.Hex(), nil
+	}
+	d.logger.Trace(user)
+	return "", fmt.Errorf("failed to convert objectID to hex: %s", oid)
+}
+
+func (d *db) FindOne(ctx context.Context, id string) (u user.User, err error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return u, fmt.Errorf("failed to convert het to objID: %s", id)
+	}
+	filter := bson.M{"_id": oid}
+	result := d.collection.FindOne(ctx, filter)
+	if result.Err() != nil {
+		// todo 404
+		return u, fmt.Errorf("failed to find one user by id: %s", id)
+	}
+	if err = result.Decode(&u); err != nil {
+		return u, fmt.Errorf("failed to decode user from db with id: %s", id)
+	}
+	return u, nil
+}
+
+func (d *db) Update(ctx context.Context, user user.User) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *db) Delete(ctx context.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewStorage(database *mongo.Database, collection string, logger *logging.Logger) user.Storage {
+	return &db{
+		collection: database.Collection(collection),
+		logger:     logger,
+	}
+}
--- internal/user/model.go
+++ internal/user/model.go
@@ -1,1 +1,14 @@
 package user
+
+type User struct {
+	ID           string `json:"id" bson:"_id,omitempty"`
+	Username     string `json:"username" bson:"username"`
+	Email        string `json:"email" bson:"email"`
+	PasswordHash string `json:"-" bson:"password"`
+}
+
+type CreateUserDTO struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
